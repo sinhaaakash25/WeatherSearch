9a310e15cb7e181cbf8992496d18c5064c8570d8
aakash sinha <aksinha2509@gmail.com>
2022-05-20 11:45:57 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Aakash Sinha on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
